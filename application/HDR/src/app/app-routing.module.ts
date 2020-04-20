0c5689e38dbdcac95b46978adb8b416077c0665e
Bruno Augusto de Souza Oliveira <brunoaugustoliveira@gmail.com>
2020-04-20 17:37:06 -0300

    Ajustes de rota e criação de novos componentes para login e cadastro de usuário

--- application/HDR/src/app/app-routing.module.ts
+++ application/HDR/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./login/login/login.module').then( m => m.LoginComponentModule)},
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
 ];
 
